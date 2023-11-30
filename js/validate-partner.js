ee2f5ad7a776da4670bc9a96563299426b7a6c09
Di Blasio <arthurdiblasio@gmail.com>
2023-11-30 18:03:35 -0300

    feat: formulário partner

--- js/validate-partner.js
+++ js/validate-partner.js
@@ -1,44 +1,46 @@
-
 (function () {
   "use strict";
 
-  let formsDoubts = document.querySelectorAll('.php-email-form-doubts');
+  let formsDoubts = document.querySelectorAll(".php-email-form-partner");
 
   formsDoubts.forEach(function (e) {
-    e.addEventListener('submit', function (event) {
+    e.addEventListener("submit", function (event) {
       event.preventDefault();
 
       let thisForm = this;
-      let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+      let action = thisForm.getAttribute("action");
+      let recaptcha = thisForm.getAttribute("data-recaptcha-site-key");
 
       if (!action) {
-        displayError(thisForm, 'The form action property is not set!')
+        displayError(thisForm, "The form action property is not set!");
         return;
       }
-      thisForm.querySelector('.loading').classList.add('d-block');
-      thisForm.querySelector('.error-message').classList.remove('d-block');
-      thisForm.querySelector('.sent-message').classList.remove('d-block');
+      thisForm.querySelector(".loading").classList.add("d-block");
+      thisForm.querySelector(".error-message").classList.remove("d-block");
+      thisForm.querySelector(".sent-message").classList.remove("d-block");
 
       let formData = new FormData(thisForm);
-
       if (recaptcha) {
         if (typeof grecaptcha !== "undefined") {
           grecaptcha.ready(function () {
             try {
-              grecaptcha.execute(recaptcha, { action: 'email_form_submit' })
-                .then(token => {
-                  formData.set('recaptcha-response', token);
+              grecaptcha
+                .execute(recaptcha, { action: "email_form_submit" })
+                .then((token) => {
+                  formData.set("recaptcha-response", token);
                   email_form_submit(thisForm, action, formData);
-                })
+                });
             } catch (error) {
-              displayError(thisForm, error)
+              displayError(thisForm, error);
             }
           });
         } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!');
+          displayError(
+            thisForm,
+            "The reCaptcha javascript API url is not loaded!"
+          );
         }
-      } else if (formInvalidDoubts(thisForm)) {
+      } else if (formInvalidPartner(thisForm)) {
         return;
       } else {
         email_form_submit(thisForm, action, formData);
@@ -46,44 +48,106 @@
     });
   });
 
-  function formInvalidDoubts(thisForm) {
-    let message = '';
-    const phone = getById('phone');
-    phone.classList.remove('error-form');
-    if (phone.value.length != 15) {
-      phone.classList.add('error-form');
-      message += 'Telefone inválido! <br>';
-    }
+  function validCNPJ(cnpj) {
+    cnpj = cnpj.replace(/[^\d]+/g, "");
 
+    if (cnpj == "") return false;
+
+    if (cnpj.length != 14) return false;
+
+    // Elimina CNPJs invalidos conhecidos
+    if (
+      cnpj == "00000000000000" ||
+      cnpj == "11111111111111" ||
+      cnpj == "22222222222222" ||
+      cnpj == "33333333333333" ||
+      cnpj == "44444444444444" ||
+      cnpj == "55555555555555" ||
+      cnpj == "66666666666666" ||
+      cnpj == "77777777777777" ||
+      cnpj == "88888888888888" ||
+      cnpj == "99999999999999"
+    )
+      return false;
+
+    // Valida DVs
+    let tamanho = cnpj.length - 2;
+    let numeros = cnpj.substring(0, tamanho);
+    let digitos = cnpj.substring(tamanho);
+    let soma = 0;
+    let pos = tamanho - 7;
+    for (let i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2) pos = 9;
+    }
+    let resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+    if (resultado != digitos.charAt(0)) return false;
+
+    tamanho = tamanho + 1;
+    numeros = cnpj.substring(0, tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+
+    for (let i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2) pos = 9;
+    }
+    resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+    if (resultado != digitos.charAt(1)) return false;
+
+    return true;
+  }
+
+  function formInvalidPartner(thisForm) {
+    let message = "";
+    const phone = getById("phone");
+    const cnpj = getById("cnpj");
+    phone.classList.remove("error-form");
+    cnpj.classList.remove("error-form");
+    if (phone.value.length != 15) {
+      phone.classList.add("error-form");
+      message += "Telefone inválido! <br>";
+    }
+    if (cnpj.value.length === 18 && !validCNPJ(cnpj.value.replace(/\D/g, ""))) {
+      cnpj.classList.add("error-form");
+      message += "CNPJ inválido! <br>";
+    }
     if (message) {
       displayError(thisForm, message);
       return true;
     }
   }
 
-
   function email_form_submit(thisForm, action, formData) {
     fetch(action, {
-      method: 'POST',
+      method: "POST",
       body: formData,
-      headers: { 'X-Requested-With': 'XMLHttpRequest' }
+      headers: { "X-Requested-With": "XMLHttpRequest" },
     })
-      .then(response => {
+      .then((response) => {
         if (response.ok) {
-          return response.status
+          return response.status;
         } else {
-          throw new Error(`${response.status} ${response.statusText} ${response.url}`);
+          throw new Error(
+            `${response.status} ${response.statusText} ${response.url}`
+          );
         }
       })
-      .then(status => {
-        thisForm.querySelector('.loading').classList.remove('d-block');
+      .then((status) => {
+        thisForm.querySelector(".loading").classList.remove("d-block");
         if (status === 200) {
-          thisForm.querySelector('.sent-message').classList.add('d-block');
+          thisForm.querySelector(".sent-message").classList.add("d-block");
           thisForm.reset();
-          const message = 'Mensagem enviada! Obrigado pela opinião!';
+          const message =
+            "Sua solicitação foi recebida, em breve entraremos em contato!";
           showSnackbar(message);
         } else {
-          throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action);
+          throw new Error(
+            data
+              ? data
+              : "Form submission failed and no error message returned from: " +
+                action
+          );
         }
       })
       .catch((error) => {
@@ -92,10 +156,10 @@
   }
 
   function displayError(thisForm, error) {
-    if (!error.ok || error.ok !== 'true') {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      thisForm.querySelector('.error-message').innerHTML = error;
-      thisForm.querySelector('.error-message').classList.add('d-block');
+    if (!error.ok || error.ok !== "true") {
+      thisForm.querySelector(".loading").classList.remove("d-block");
+      thisForm.querySelector(".error-message").innerHTML = error;
+      thisForm.querySelector(".error-message").classList.add("d-block");
     }
   }
 
@@ -103,13 +167,13 @@
   function getInputMask(o, f) {
     const v_obj = o;
     const v_fun = f;
-    v_obj.value = v_fun(v_obj.value)
+    v_obj.value = v_fun(v_obj.value);
   }
 
   function maskTel(value) {
     value = value.replace(/\D/g, ""); //Remove tudo o que não é dígito
     if (value.length > 11) {
-      value = value.substr(0, 11)
+      value = value.substr(0, 11);
     }
 
     value = value.replace(/^(\d{2})(\d)/g, "($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
@@ -117,31 +181,34 @@
     return value;
   }
 
+  function maskCNPJ(value) {
+    value = value.replace(/\D/g, "");
+    if (value.length > 14) {
+      value = value.substr(0, 14);
+    }
+    value = value.replace(
+      /^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/g,
+      "$1.$2.$3/$4-$5"
+    );
+    return value;
+  }
+
   function inputOnlyLetters(value) {
-    return value.replace(/[0-9]/g, '');
+    return value.replace(/[0-9]/g, "");
   }
 
   function getById(el) {
     return document.getElementById(el);
   }
 
-  getById('phone').onkeyup = function () {
-    console.log('resr');
+  getById("cnpj").onkeyup = function () {
+    getInputMask(this, maskCNPJ);
+  };
+
+  getById("phone").onkeyup = function () {
     getInputMask(this, maskTel);
-  }
-  getById('name').onkeyup = function () {
-    getInputMask(this, inputOnlyLetters);
-  }
+  };
 
-  getById('name-indicated').onkeyup = function () {
-    getInputMask(this, inputOnlyLetters);
-  }
-
-  getById('digit').onkeyup = function () {
-    this.value = this.value.substring(0, 1)
-  }
-
-  
   function showSnackbar(message) {
     const snackbarDiv = document.getElementById("snackbar");
 
@@ -150,7 +217,8 @@
     snackbarDiv.innerText = message;
 
     // After 3 seconds, remove the show class from DIV
-    setTimeout(function () { snackbarDiv.className = snackbarDiv.className.replace("show", ""); }, 3000);
+    setTimeout(function () {
+      snackbarDiv.className = snackbarDiv.className.replace("show", "");
+    }, 3000);
   }
-
 })();
